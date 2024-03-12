aacd10986b9a45204ce7745deef64360a82c9c5a
Max Alva <alva85@gmail.com>
2024-03-12 09:52:35 -0500

    Implementar métodos faltantes

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Delete, Get, Inject, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 import { PRODUCT_SERVICE } from '../config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError, Observable } from 'rxjs';
 import { PaginationDto } from '../common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller( 'products' )
 export class ProductsController {
@@ -10,8 +12,8 @@ export class ProductsController {
   }
 
   @Post()
-  createProduct( @Body() body: any ) {
-    return 'This action adds a new product';
+  createProduct( @Body() createProductDto: CreateProductDto ) {
+    return this.productsClient.send( { cmd: 'create_product' }, createProductDto );
   }
 
   @Get()
@@ -27,22 +29,28 @@ export class ProductsController {
           throw new RpcException( error );
         } )
       );
-    /*try {
-      return await firstValueFrom( this.productsClient.send( { cmd: 'find_one_product' }, { id } ) );
-    } catch ( error ) {
-      throw new RpcException( error );
-    }*/
   }
 
   @Delete( ':id' )
   deleteProduct( @Param( 'id' ) id: string ) {
-    return `This action removes a #${ id } product`;
+    return this.productsClient.send( { cmd: 'delete_product' }, { id } )
+      .pipe(
+        catchError( error => {
+          throw new RpcException( error );
+        } )
+      );
   }
 
   @Patch( ':id' )
   updateProduct(
-    @Param( 'id' ) id: string,
-    @Body() body: any ) {
-    return `This action updates a #${ id } product`;
+    @Param( 'id', ParseIntPipe ) id: string,
+    @Body() updateProductDto: UpdateProductDto ) {
+    console.log( { id, ...updateProductDto } );
+    return this.productsClient.send( { cmd: 'update_product' }, { id, ...updateProductDto } )
+      .pipe(
+        catchError( error => {
+          throw new RpcException( error );
+        } )
+      );
   }
 }
